a4fe8ccba182ef1e2d14ad36948d77fa3aba4468
AtmoOmen <atmoomen2004@outlook.com>
2023-12-31 15:43:32 +0800

    CheckboxColored

--- Global.cs
+++ Global.cs
@@ -2,3 +2,4 @@
 global using System.Numerics;
 global using Dalamud.Interface;
 global using Dalamud.Interface.Utility;
+global using Dalamud.Interface.Colors;
---
+++ ImGuiOm/Checkbox.cs
@@ -0,0 +1,23 @@
+﻿namespace OmenTools.ImGuiOm;
+
+public class Checkbox
+{
+    /// <summary>
+    /// Draw a checkbox with a colored label that indicates whether the checkbox is checked.
+    /// </summary>
+    /// <param name="label"></param>
+    /// <param name="selected"></param>
+    /// <param name="enabledColor">If left null, the color will be ImGuiColors.DalamudYellow</param>
+    /// <param name="disabledColor">If left null, the color will be ImGuiColors.DalamudWhite</param>
+    /// <returns></returns>
+    public static bool CheckboxColored(string label, ref bool selected, Vector4? enabledColor = null, Vector4? disabledColor = null)
+    {
+        var color1 = enabledColor ?? ImGuiColors.DalamudYellow;
+        var color2 = disabledColor ?? ImGuiColors.DalamudWhite;
+        ImGui.PushStyleColor(ImGuiCol.Text, selected ? color1 : color2);
+        var result = ImGui.Checkbox(label, ref selected);
+        ImGui.PopStyleColor();
+
+        return result;
+    }
+}
