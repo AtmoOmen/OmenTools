d46b02115569b44ef2325193ea286d1a4e809877
AtmoOmen <atmoomen2004@outlook.com>
2023-12-31 15:56:32 +0800

    HelpMarker

--- ImGuiOm/Widgets.cs
+++ ImGuiOm/Widgets.cs
@@ -2,5 +2,20 @@
 
 public static partial class ImGuiOm
 {
+    public static void HelpMarker(string tooltip, float warpPos = 20f, FontAwesomeIcon icon = FontAwesomeIcon.InfoCircle)
+    {
+        ImGui.SameLine();
+        ImGui.PushFont(UiBuilder.IconFont);
+        ImGui.TextDisabled(FontAwesomeIcon.InfoCircle.ToIconString());
+        ImGui.PopFont();
+        if (ImGui.IsItemHovered())
+        {
+            ImGui.BeginTooltip();
+            ImGui.PushTextWrapPos(ImGui.GetFontSize() * warpPos);
+            ImGui.TextUnformatted(tooltip);
+            ImGui.PopTextWrapPos();
+            ImGui.EndTooltip();
+        }
+    }
 
 }
