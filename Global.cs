10d1facce78504de67408f77c3d7d7859074e5b2
AtmoOmen <atmoomen2004@outlook.com>
2024-01-16 18:00:18 +0800

    UTF8StringToString

--- Global.cs
+++ Global.cs
@@ -4,6 +4,8 @@ global using Dalamud.Interface;
 global using Dalamud.Interface.Utility;
 global using Dalamud.Interface.Colors;
 global using Dalamud.Utility;
+global using System.Text;
+global using FFXIVClientStructs.FFXIV.Client.System.String;
 global using Dalamud.Interface.Utility.Raii;
 global using FFXIVClientStructs.FFXIV.Component.GUI;
 global using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
---
+++ Helpers/Common.cs
@@ -0,0 +1,11 @@
+﻿namespace OmenTools.Helpers;
+
+public static partial class HelpersOm
+{
+    public static unsafe string UTF8StringToString(Utf8String str)
+    {
+        if (str.StringPtr == null || str.BufUsed <= 1)
+            return string.Empty;
+        return Encoding.UTF8.GetString(str.StringPtr, (int)str.BufUsed - 1);
+    }
+}
