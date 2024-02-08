558dfb399cbfbe7eaf91e99d04da13a95a1d566e
AtmoOmen <atmoomen2004@outlook.com>
2024-02-08 11:26:34 +0800

    ButtonSelectable

--- ImGuiOm/Button.cs
+++ ImGuiOm/Button.cs
@@ -86,4 +86,22 @@ public static partial class ImGuiOm
 
         return result;
     }
+
+    public static bool ButtonSelectable(string text)
+    {
+        var style = ImGui.GetStyle();
+        var padding = style.FramePadding;
+        var colors = style.Colors;
+        var textSize = ImGui.CalcTextSize(text);
+
+        var size = new Vector2(Math.Max(ImGui.GetContentRegionAvail().X, textSize.X + (2 * padding.X)), textSize.Y + (2 * padding.Y));
+
+        ImGui.PushStyleColor(ImGuiCol.ButtonActive, colors[(int)ImGuiCol.HeaderActive]);
+        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, colors[(int)ImGuiCol.HeaderHovered]);
+        ImGui.PushStyleColor(ImGuiCol.Button, 0);
+        var result = ImGui.Button(text, size);
+        ImGui.PopStyleColor(3);
+
+        return result;
+    }
 }
