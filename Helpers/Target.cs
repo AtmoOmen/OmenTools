46b922b57daf0531eecf8d9d821a9c17b9758078
AtmoOmen <atmoomen2004@outlook.com>
2024-02-10 17:37:24 +0800

    Update Target.cs

--- Helpers/Target.cs
+++ Helpers/Target.cs
@@ -4,7 +4,14 @@ public static partial class HelpersOm
 {
     public static unsafe bool CanUseActionOnObject(GameObject* player, GameObject* target, float actionRange)
     {
-        return !(GetGameDistanceFromObject(player, target) - 0.5 > actionRange);
+        var actionRangeSquared = (actionRange + 0.5f) * (actionRange + 0.5f);
+        return GetSquareDistanceFromObject(player, target) <= actionRangeSquared;
+    }
+
+    public static unsafe float GetSquareDistanceFromObject(GameObject* player, GameObject* target)
+    {
+        var distance = player->Position - target->Position;
+        return (distance.X * distance.X) + (distance.Z * distance.Z) - 2 * target->HitboxRadius * target->HitboxRadius;
     }
 
     public static unsafe float GetGameDistanceFromObject(GameObject* player, GameObject* target)
