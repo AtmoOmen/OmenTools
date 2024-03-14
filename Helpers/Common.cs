3406cc7016cefe81ddb49ba7be5ec93e6e3275b3
AtmoOmen <atmoomen2004@outlook.com>
2024-03-14 13:08:09 +0800

    Update Common.cs

--- Helpers/Common.cs
+++ Helpers/Common.cs
@@ -2,16 +2,21 @@
 
 namespace OmenTools.Helpers;
 
-public static partial class HelpersOm
+public static unsafe partial class HelpersOm
 {
-    public static unsafe string UTF8StringToString(Utf8String str)
+    public static string UTF8StringToString(Utf8String str)
     {
         if (str.StringPtr == null || str.BufUsed <= 1)
             return string.Empty;
         return Encoding.UTF8.GetString(str.StringPtr, (int)str.BufUsed - 1);
     }
 
-    public static unsafe string FetchText(this Utf8String s, bool onlyFirst = false)
+    public static string FetchText(this Lumina.Text.SeString s, bool onlyFirst = false)
+    {
+        return s.ToDalamudString().FetchText(onlyFirst);
+    }
+
+    public static string FetchText(this Utf8String s, bool onlyFirst = false)
     {
         var str = MemoryHelper.ReadSeString(&s);
         return str.FetchText();
