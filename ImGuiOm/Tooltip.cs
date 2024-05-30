978c85650070e78fb35dd75c8961ed16af8fc48a
AtmoOmen <atmoomen2004@outlook.com>
2024-05-30 12:35:03 +0800

    TooltipDisableHelp

--- ImGuiOm/Tooltip.cs
+++ ImGuiOm/Tooltip.cs
@@ -17,4 +17,18 @@ public static partial class ImGuiOm
         }
         ImGui.PopID();
     }
+
+    public static void TooltipDisableHelp(List<KeyValuePair<bool, string>> conditions, string header = "Disabled for the following reasons")
+    {
+        var tooltips = (from condition in conditions where condition.Key select condition.Value).ToList();
+
+        if (tooltips.Count <= 0) return;
+        if (!ImGui.IsItemHovered()) return;
+
+        ImGui.BeginTooltip();
+        ImGui.TextColored(ImGuiColors.DalamudYellow, $"{header}:");
+        for (var i = 0; i < tooltips.Count; i++)
+            ImGui.Text($"{i + 1}. {tooltips[i]}");
+        ImGui.EndTooltip();
+    }
 }
