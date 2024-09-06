2dc482f8491b689ba1d7706d9868db7c816b99cc
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2024-09-06 15:42:58 +0800

    Create Service.cs

---
+++ Service.cs
@@ -0,0 +1,57 @@
+﻿using Dalamud.Game.ClientState.Objects;
+using Dalamud.Interface;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace OmenTools;
+
+public class Service
+{
+    public void Init(IDalamudPluginInterface pluginInterface)
+    {
+        PluginInterface = pluginInterface;
+        UiBuilder       = pluginInterface.UiBuilder;
+        
+        pluginInterface.Create<Service>();
+    }
+    
+    [PluginService] public static IAddonLifecycle      AddonLifecycle  { get; private set; } = null!;
+    [PluginService] public static IAddonEventManager   AddonEvent      { get; private set; } = null!;
+    [PluginService] public static IAetheryteList       AetheryteList   { get; private set; } = null!;
+    [PluginService] public static IBuddyList           BuddyList       { get; private set; } = null!;
+    [PluginService] public static IChatGui             Chat            { get; private set; } = null!;
+    [PluginService] public static IClientState         ClientState     { get; private set; } = null!;
+    [PluginService] public static ICommandManager      Command         { get; private set; } = null!;
+    [PluginService] public static ICondition           Condition       { get; private set; } = null!;
+    [PluginService] public static IContextMenu         ContextMenu     { get; private set; } = null!;
+    [PluginService] public static IDataManager         Data            { get; private set; } = null!;
+    [PluginService] public static IDtrBar              DtrBar          { get; private set; } = null!;
+    [PluginService] public static IDutyState           DutyState       { get; private set; } = null!;
+    [PluginService] public static IFateTable           Fate            { get; private set; } = null!;
+    [PluginService] public static IFlyTextGui          FlyText         { get; private set; } = null!;
+    [PluginService] public static IFramework           Framework       { get; private set; } = null!;
+    [PluginService] public static IGameConfig          GameConfig      { get; private set; } = null!;
+    [PluginService] public static IGameGui             Gui             { get; private set; } = null!;
+    [PluginService] public static IGameInteropProvider Hook            { get; private set; } = null!;
+    [PluginService] public static IGameInventory       Inventory       { get; private set; } = null!;
+    [PluginService] public static IGameLifecycle       Lifecycle       { get; private set; } = null!;
+    [PluginService] public static IGameNetwork         Network         { get; private set; } = null!;
+    [PluginService] public static IGamepadState        Gamepad         { get; private set; } = null!;
+    [PluginService] public static IJobGauges           JobGauges       { get; private set; } = null!;
+    [PluginService] public static IKeyState            KeyState        { get; private set; } = null!;
+    [PluginService] public static IMarketBoard         MarketBoard     { get; private set; } = null!;
+    [PluginService] public static INamePlateGui        NamePlateGui    { get; private set; } = null!;
+    [PluginService] public static INotificationManager DalamudNotice   { get; private set; } = null!;
+    [PluginService] public static IObjectTable         ObjectTable     { get; private set; } = null!;
+    [PluginService] public static IPartyFinderGui      PartyFinder     { get; private set; } = null!;
+    [PluginService] public static IPartyList           PartyList       { get; private set; } = null!;
+    [PluginService] public static IPluginLog           Log             { get; private set; } = null!;
+    [PluginService] public static ITargetManager       Target          { get; private set; } = null!;
+    [PluginService] public static ITextureProvider     Texture         { get; private set; } = null!;
+    [PluginService] public static ITitleScreenMenu     TitleScreenMenu { get; private set; } = null!;
+    [PluginService] public static IToastGui            Toast           { get; private set; } = null!;
+
+    public static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    public static IUiBuilder              UiBuilder       { get; private set; } = null!;
+}
