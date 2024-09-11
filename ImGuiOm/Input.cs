0f0ff6c87f2d92383c55c1fab0ad1ecad4d34c68
AtmoOmen <atmoomen2004@outlook.com>
2024-09-11 09:45:20 +0800

    Create Input.cs

---
+++ ImGuiOm/Input.cs
@@ -0,0 +1,63 @@
+﻿using ImGuiNET;
+
+namespace OmenTools.ImGuiOm;
+
+public static partial class ImGuiOm
+{
+    public static bool InputLabelLeft(string label, Func<bool> origInputFunc)
+    {
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text(label);
+
+        ImGui.SameLine();
+        return origInputFunc();
+    }
+
+    public static bool InputUInt(string label, ref uint v)
+    {
+        var tempV = (int)v;
+        var result = ImGui.InputInt(label, ref tempV);
+        if (result)
+        {
+            tempV = Math.Max(0, tempV);
+            v = (uint)tempV;
+        }
+        return result;
+    }
+
+    public static bool InputUInt(string label, ref uint v, int step)
+    {
+        var tempV = (int)v;
+        var result = ImGui.InputInt(label, ref tempV, step);
+        if (result)
+        {
+            tempV = Math.Max(0, tempV);
+            v = (uint)tempV;
+        }
+        return result;
+    }
+
+    public static bool InputUInt(string label, ref uint v, int step, int step_fast)
+    {
+        var tempV = (int)v;
+        var result = ImGui.InputInt(label, ref tempV, step, step_fast);
+        if (result)
+        {
+            tempV = Math.Max(0, tempV);
+            v = (uint)tempV;
+        }
+        return result;
+    }
+
+    public static bool InputUInt(string label, ref uint v, int step, int step_fast, ImGuiInputTextFlags flags)
+    {
+        var tempV = (int)v;
+        var result = ImGui.InputInt(label, ref tempV, step, step_fast, flags);
+        if (result)
+        {
+            tempV = Math.Max(0, tempV);
+            v = (uint)tempV;
+        }
+        return result;
+    }
+}
