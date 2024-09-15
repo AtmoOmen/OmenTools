3df560f1028add14ee20c4591ab818fa82c24aee
AtmoOmen <atmoomen2004@outlook.com>
2024-09-15 14:28:46 +0800

    1

--- Helpers/Agent.cs
+++ Helpers/Agent.cs
@@ -1,38 +1,35 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using OmenTools.Infos;
-using System.Runtime.InteropServices;
 
 namespace OmenTools.Helpers;
 
 public static unsafe partial class HelpersOm
 {
+    private static readonly Dictionary<AgentId, nint> CachedAgents = [];
+
     public static AtkValue* SendEvent(AgentId agentId, ulong eventKind, params object[] eventparams)
     {
-        var agent = AgentModule.Instance()->GetAgentByInternalId(agentId);
-        return agent == null ? null : SendEvent(agent, eventKind, eventparams);
+        if (!CachedAgents.TryGetValue(agentId, out var agent))
+        {
+            var newAgent = (nint)AgentModule.Instance()->GetAgentByInternalId(agentId);
+            CachedAgents[agentId] = newAgent;
+            agent = newAgent;
+        }
+
+        return agent == nint.Zero ? null : SendEvent((AgentInterface*)agent, eventKind, eventparams);
     }
 
     public static AtkValue* SendEvent(AgentInterface* agentInterface, ulong eventKind, params object[] eventParams)
     {
-        var eventObject = stackalloc AtkValue[1];
-        return SendEvent(agentInterface, eventObject, eventKind, eventParams);
+        var returnValue = stackalloc AtkValue[1];
+        return SendEvent(agentInterface, returnValue, eventKind, eventParams);
     }
 
-    public static AtkValue* SendEvent(AgentInterface* agentInterface, AtkValue* eventObject, ulong eventKind, params object[] eventParams)
+    public static AtkValue* SendEvent(AgentInterface* agentInterface, AtkValue* returnValue, ulong eventKind, params object[] eventParams)
     {
         using var atkValues = new AtkValueArray(eventParams);
-        agentInterface->ReceiveEvent(eventObject, atkValues, (uint)eventParams.Length, eventKind);
-        return eventObject;
-    }
-
-    [StructLayout(LayoutKind.Explicit, Size = 64)]
-    public struct EventObject
-    {
-        [FieldOffset(0)]
-        public ulong Unknown0;
-
-        [FieldOffset(8)]
-        public ulong Unknown8;
+        agentInterface->ReceiveEvent(returnValue, atkValues, (uint)eventParams.Length, eventKind);
+        return returnValue;
     }
 }
