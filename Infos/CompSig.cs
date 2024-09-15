980fead4182a842a22720a8f004668eb1cb3f07d
AtmoOmen <atmoomen2004@outlook.com>
2024-09-15 12:34:48 +0800

    CompSig

---
+++ Infos/CompSig.cs
@@ -0,0 +1,22 @@
+﻿using Dalamud.Game;
+
+namespace OmenTools.Infos;
+
+/// <summary>
+/// Composite Signatures 复合签名
+/// </summary>
+public record CompSig(string Signature, string? SignatureCN = null)
+{
+    public static bool IsClientCN => DService.ClientState.ClientLanguage == (ClientLanguage)4;
+
+    public string? Get() => TryGet(out var sig) ? sig : null;
+
+    public bool TryGet(out string? signature)
+    {
+        signature = !string.IsNullOrWhiteSpace(Signature) && IsClientCN && !string.IsNullOrWhiteSpace(SignatureCN)
+            ? SignatureCN
+            : Signature;
+
+        return !string.IsNullOrWhiteSpace(signature);
+    }
+}
