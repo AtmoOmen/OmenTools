dd098603372f15b7a1c3be8598c3ef1c3606f3da
AtmoOmen <atmoomen2004@outlook.com>
2024-09-18 16:08:35 +0800

    AgentReceiveEventDelegate

---
+++ Infos/Delegates.cs
@@ -0,0 +1,10 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace OmenTools.Infos;
+
+public static unsafe partial class InfosOm
+{
+    public delegate AtkValue* AgentReceiveEventDelegate(
+        AgentInterface* agent, AtkValue* returnValues, AtkValue* values, uint valueCount, ulong eventKind);
+}
