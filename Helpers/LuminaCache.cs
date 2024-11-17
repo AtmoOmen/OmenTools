6dced4e9810538eb685b01c82a0e9120948cf24a
AtmoOmen <atmoomen2004@outlook.com>
2024-11-17 15:20:07 +0800

    更新 LuminaCache

--- Helpers/LuminaCache.cs
+++ Helpers/LuminaCache.cs
@@ -4,21 +4,42 @@ namespace OmenTools.Helpers;
 
 public static class LuminaCache
 {
-    public static ExcelSheet<T>? Get<T>() where T : ExcelRow 
-        => DService.Data.GetExcelSheet<T>();
-
-    public static bool TryGet<T>(out ExcelSheet<T>? sheet) where T : ExcelRow
+    public static ExcelSheet<T>? Get<T>() where T : struct, IExcelRow<T>
     {
-        sheet = DService.Data.GetExcelSheet<T>();
+        try
+        {
+            return DService.Data.GetExcelSheet<T>();
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static bool TryGet<T>(out ExcelSheet<T>? sheet) where T : struct, IExcelRow<T>
+    {
+        sheet = Get<T>();
         return sheet != null;
     }
 
-    public static T? GetRow<T>(uint rowID) where T : ExcelRow 
-        => DService.Data.GetExcelSheet<T>()?.GetRow(rowID);
-
-    public static bool TryGetRow<T>(uint rowID, out T? item) where T : ExcelRow
+    public static T? GetRow<T>(uint rowID) where T : struct, IExcelRow<T>
     {
-        item = DService.Data.GetExcelSheet<T>()?.GetRow(rowID);
-        return item != null;
+        var sheet = TryGet<T>(out var excelSheet) ? excelSheet : null;
+        if (sheet == null) return null;
+
+        try
+        {
+            return sheet.GetRow(rowID);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static bool TryGetRow<T>(uint rowID, out T? item) where T : struct, IExcelRow<T>
+    {
+        item = GetRow<T>(rowID);
+        return item.HasValue;
     }
 }
