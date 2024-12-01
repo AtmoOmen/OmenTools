ef4fcab23037a5e1929e439f022ada061f59a682
AtmoOmen <atmoomen2004@outlook.com>
2024-12-01 16:23:57 +0800

    还是可空吧

--- Helpers/LuminaCache.cs
+++ Helpers/LuminaCache.cs
@@ -22,45 +22,27 @@ public static class LuminaCache
         return true;
     }
 
-    public static T GetRow<T>(uint rowID) where T : struct, IExcelRow<T>
+    public static T? GetRow<T>(uint rowID) where T : struct, IExcelRow<T>
     {
         var sheet = TryGet<T>(out var excelSheet) ? excelSheet : null;
-        if (sheet == null) return default;
-        
-        try
-        {
-            return sheet.GetRow(rowID);
-        }
-        catch
-        {
-            return default;
-        }
+        return sheet?.GetRowOrDefault(rowID);
     }
 
-    public static T GetSubRow<T>(uint rowID) where T : struct, IExcelSubrow<T>
+    public static T? GetSubRow<T>(uint rowID) where T : struct, IExcelSubrow<T>
     {
         var sheet = TryGetSub<T>(out var excelSheet) ? excelSheet : null;
-        if (sheet == null) return default;
-
-        try
-        {
-            return sheet.GetRow(rowID).FirstOrDefault();
-        }
-        catch
-        {
-            return default;
-        }
+        return sheet?.GetRowOrDefault(rowID)?.FirstOrDefault();
     }
 
-    public static bool TryGetRow<T>(uint rowID, out T item) where T : struct, IExcelRow<T>
+    public static bool TryGetRow<T>(uint rowID, out T? item) where T : struct, IExcelRow<T>
     {
         item = GetRow<T>(rowID);
-        return item.RowId != default(T).RowId;
+        return item != null;
     }
 
-    public static bool TryGetSubRow<T>(uint rowID, out T item) where T : struct, IExcelSubrow<T>
+    public static bool TryGetSubRow<T>(uint rowID, out T? item) where T : struct, IExcelSubrow<T>
     {
         item = GetSubRow<T>(rowID);
-        return item.RowId != default(T).RowId;
+        return item != null;
     }
 }
