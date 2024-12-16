de27bbc536d00df79834e02fe6442977571f5e9c
AtmoOmen <atmoomen2004@outlook.com>
2024-12-16 11:48:46 +0800

    Dalamud Reflector

--- Helpers/Extension.cs
+++ Helpers/Extension.cs
@@ -32,6 +32,48 @@ public static unsafe partial class HelpersOm
 {
     private static readonly CompareInfo    s_compareInfo    = CultureInfo.InvariantCulture.CompareInfo;
     private const           CompareOptions s_compareOptions = CompareOptions.IgnoreCase;
+    
+    public static object? GetFoP(this object obj, string name) =>
+        obj.GetType().GetField(name, BindingAllFlags)?.GetValue(obj)
+        ?? obj.GetType().GetProperty(name, BindingAllFlags)?.GetValue(obj);
+    
+    public static T? GetFoP<T>(this object obj, string name) => (T?)GetFoP(obj, name);
+    
+    public static void SetFoP(this object obj, string name, object value)
+    {
+        var field = obj.GetType().GetField(name, BindingAllFlags);
+        if (field != null)
+            field.SetValue(obj, value);
+        else
+            obj.GetType().GetProperty(name, BindingAllFlags)?.SetValue(obj, value);
+    }
+
+    public static object? GetStaticFoP(this object obj, string type, string name) =>
+        obj.GetType().Assembly.GetType(type)?.GetField(name, BindingStaticFlags)?.GetValue(null)
+        ?? obj.GetType().Assembly.GetType(type)?.GetProperty(name, BindingStaticFlags)?.GetValue(null);
+
+    public static T? GetStaticFoP<T>(this object obj, string type, string name) 
+        => (T?)GetStaticFoP(obj, type, name);
+
+    public static void SetStaticFoP(this object obj, string type, string name, object value)
+    {
+        var field = obj.GetType().Assembly.GetType(type)?.GetField(name, BindingStaticFlags);
+        if (field != null)
+            field.SetValue(null, value);
+        else
+            obj.GetType().Assembly.GetType(type)?.GetProperty(name, BindingStaticFlags)?.SetValue(null, value);
+    }
+
+    public static object? Call(this object obj, string name, object[] @params, bool matchExactArgumentTypes = false)
+    {
+        var info = !matchExactArgumentTypes
+                       ? obj.GetType().GetMethod(name, BindingAllFlags)
+                       : obj.GetType().GetMethod(name, BindingAllFlags, @params.Select(x => x.GetType()).ToArray());
+        return info?.Invoke(obj, @params);
+    }
+
+    public static T? Call<T>(this object obj, string name, object[] @params, bool matchExactArgumentTypes = false) 
+        => (T?)Call(obj, name, @params, matchExactArgumentTypes);
 
     public static Vector2 GetPositionWorld(this Aetheryte aetheryte)
     {
@@ -315,7 +357,7 @@ public static unsafe partial class HelpersOm
     public static Vector3 ToVector3(this Vector2 vector2) 
         => vector2.ToVector3(DService.ClientState.LocalPlayer?.Position.Y ?? 0);
     
-    public static unsafe bool TargetInteract(this IGameObject? gameObject)
+    public static bool TargetInteract(this IGameObject? gameObject)
     {
         if (gameObject == null) return false;
         DService.Targets.Target = gameObject;
@@ -345,7 +387,7 @@ public static unsafe partial class HelpersOm
         }
     }
 
-    public static unsafe bool Interact(this IGameObject? gameObject) 
+    public static bool Interact(this IGameObject? gameObject) 
         => gameObject != null && TargetSystem.Instance()->InteractWithObject(gameObject.ToStruct()) != 0;
 
     public static IGameObject? FindNearest(this IEnumerable<IGameObject> gameObjects, 
