e5ae092e2dcc50395ed07ba8dc57dc663a5aea5b
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-03-08 23:06:19 +0800

    Restore

--- Infos/AtkValueArray.cs
+++ Infos/AtkValueArray.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+﻿﻿using System.Runtime.InteropServices;
 using System.Text;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using ValueType = FFXIVClientStructs.FFXIV.Component.GUI.ValueType;
@@ -7,21 +7,21 @@ namespace OmenTools.Infos;
 
 public unsafe class AtkValueArray : IDisposable
 {
-    public nint      Address { get; }
-    public AtkValue* Pointer { get; }
-    public int       Length  { get; }
-
-    public static implicit operator AtkValue*(AtkValueArray arr) => arr.Pointer;
-    
     public AtkValueArray(params object[] values)
     {
-        Length  = values.Length;
+        Length = values.Length;
         Address = Marshal.AllocHGlobal(Length * sizeof(AtkValue));
         Pointer = (AtkValue*)Address;
 
         for (var i = 0; i < Length; i++)
             EncodeValue(i, values[i]);
     }
+
+    public IntPtr    Address { get; }
+    public AtkValue* Pointer { get; }
+    public int       Length  { get; }
+
+    public static implicit operator AtkValue*(AtkValueArray arr) => arr.Pointer;
 
     public void Dispose()
     {
@@ -37,28 +37,28 @@ public unsafe class AtkValueArray : IDisposable
         switch (value)
         {
             case uint uintValue:
-                Pointer[index].SetUInt(uintValue);
+                Pointer[index] = new AtkValue { Type = ValueType.UInt, UInt = uintValue };
                 break;
             case int intValue:
-                Pointer[index].SetInt(intValue);
+                Pointer[index] = new AtkValue { Type = ValueType.Int, Int = intValue };
                 break;
             case float floatValue:
-                Pointer[index].SetFloat(floatValue);
+                Pointer[index] = new AtkValue { Type = ValueType.Float, Float = floatValue };
                 break;
             case bool boolValue:
-                Pointer[index].SetBool(boolValue);
+                Pointer[index] = new AtkValue { Type = ValueType.Bool, Byte = Convert.ToByte(boolValue) };
                 break;
             case string stringValue:
                 var stringBytes = Encoding.UTF8.GetBytes(stringValue + '\0');
                 var stringAlloc = Marshal.AllocHGlobal(stringBytes.Length);
                 Marshal.Copy(stringBytes, 0, stringAlloc, stringBytes.Length);
-                Pointer[index].SetString((byte*)stringAlloc);
+                Pointer[index] = new AtkValue { Type = ValueType.String, String = (byte*)stringAlloc };
                 break;
             case AtkValue atkValue:
                 Pointer[index] = atkValue;
                 break;
             default:
-                throw new ArgumentException($"无法将类型 {value.GetType()} 转换为 AtkValue");
+                throw new ArgumentException($"Unable to convert type {value.GetType()} to AtkValue");
         }
     }
 }
