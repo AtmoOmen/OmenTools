ab651f61eee8983f605e0afadeb12995aa722a06
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-03-12 13:15:19 +0800

    NodeState

---
+++ Infos/NodeState.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace OmenTools.Infos;
+
+public class NodeState
+{
+    public Vector2 Position;
+    public Vector2 Position2;
+    public Vector2 Size;
+    public bool    Visible;
+
+    public static unsafe NodeState Get(AtkResNode* node)
+    {
+        var position = GetNodePosition(node);
+        var scale    = GetNodeScale(node);
+        var size     = new Vector2(node->Width, node->Height) * scale;
+
+        return new NodeState()
+        {
+            Position  = position,
+            Position2 = position + size,
+            Visible   = GetNodeVisible(node),
+            Size      = size,
+        };
+    }
+}
