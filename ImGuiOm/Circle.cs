89bf30e802ad7742d977233f5fc342c9ca747a83
AtmoOmen <atmoomen2004@outlook.com>
2025-04-03 13:36:42 +0800

    更新俩描边方法绘制逻辑

--- ImGuiOm/Circle.cs
+++ ImGuiOm/Circle.cs
@@ -7,13 +7,19 @@ public static partial class ImGuiOm
 {
     public static void CircleOutlined(
         Vector2 center, float radius, uint fillColor, uint outlineColor = 0xFF000000, float outlineThickness = 1.5f,
-        ImDrawListPtr? drawList = null)
+        float opacity = 1f, ImDrawListPtr? drawList = null)
     {
         drawList ??= ImGui.GetBackgroundDrawList();
 
+        // 不透明度
+        var fillAlpha = (byte)(((fillColor >> 24) & 0xFF) * opacity);
+        var outlineAlpha = (byte)(((outlineColor >> 24) & 0xFF) * opacity);
+        var fillColorWithOpacity = (fillColor & 0x00FFFFFF) | ((uint)fillAlpha << 24);
+        var outlineColorWithOpacity = (outlineColor & 0x00FFFFFF) | ((uint)outlineAlpha << 24);
+
         // 描边
-        drawList?.AddCircleFilled(center, radius + outlineThickness, outlineColor);
+        drawList?.AddCircleFilled(center, radius + outlineThickness, outlineColorWithOpacity);
         // 原始
-        drawList?.AddCircleFilled(center, radius, fillColor);
+        drawList?.AddCircleFilled(center, radius, fillColorWithOpacity);
     }
 }
