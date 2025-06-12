008baf42dcc5fe96ee6d314352df9b304504f2f1
AtmoOmen <atmoomen2004@outlook.com>
2025-06-12 20:10:58 +0800

    一些杂项

--- Infos/Common.cs
+++ Infos/Common.cs
@@ -11,4 +11,25 @@ public static partial class InfosOm
         InventoryType.Inventory3, 
         InventoryType.Inventory4
     ];
+
+    public static readonly List<InventoryType> PlayerArmoryInventories =
+    [
+        InventoryType.EquippedItems,
+        InventoryType.ArmoryMainHand,
+        InventoryType.ArmoryOffHand,
+        InventoryType.ArmoryHead,
+        InventoryType.ArmoryBody,
+        InventoryType.ArmoryHands,
+        InventoryType.ArmoryWaist,
+        InventoryType.ArmoryLegs,
+        InventoryType.ArmoryFeets,
+        InventoryType.ArmoryEar,
+        InventoryType.ArmoryNeck,
+        InventoryType.ArmoryWrist,
+        InventoryType.ArmoryRings,
+        InventoryType.Inventory1,
+        InventoryType.Inventory2,
+        InventoryType.Inventory3,
+        InventoryType.Inventory4
+    ];
 }
