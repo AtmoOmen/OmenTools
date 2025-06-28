537452513e9f67f6548c2e2a538bc0857c18279b
AtmoOmen <atmoomen2004@outlook.com>
2025-06-28 16:08:27 +0800

    LocalPlayerState

--- Infos/LocalPlayerState.cs
+++ Infos/LocalPlayerState.cs
@@ -1,5 +1,6 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using Lumina.Excel.Sheets;
 
 namespace OmenTools.Infos;
@@ -65,8 +66,61 @@ public unsafe class LocalPlayerState
         PlayerState.Instance()->IsLevelSynced != 0;
 
     /// <summary>
+    /// 当前玩家对象
+    /// </summary>
+    public static IPlayerCharacter? Object =>
+        DService.ObjectTable.LocalPlayer;
+
+    /// <summary>
     /// 获取当前玩家指定职业的等级
     /// </summary>
     public static ushort GetClassJobLevel(uint classJobID, bool checkParentJob = true) =>
         ClassJob == classJobID ? CurrentLevel : GetClassJobLevelInternal(PlayerState.Instance(), classJobID, checkParentJob);
+
+    /// <summary>
+    /// 获取当前玩家第一个可用的职业套装
+    /// </summary>
+    public static bool TryFindClassJobGearset(uint classJobID, out byte gearsetID)
+    {
+        gearsetID = 0;
+        
+        var gearsetModule = RaptureGearsetModule.Instance();
+        for (var i = 0; i < 100; i++)
+        {
+            var gearset = gearsetModule->GetGearset(i);
+            if (gearset == null                                                          ||
+                !gearset->Flags.HasFlag(RaptureGearsetModule.GearsetFlag.Exists)         ||
+                gearset->Flags.HasFlag(RaptureGearsetModule.GearsetFlag.MainHandMissing) ||
+                gearset->Id       != i                                                   ||
+                gearset->ClassJob != classJobID)
+                continue;
+
+            gearsetID = gearset->Id;
+            return true;
+        }
+            
+        return false;
+    }
+
+    public static bool SwitchGearset(uint classJob)
+    {
+        if (Object == null || !TryFindClassJobGearset(classJob, out var gearsetID)) return false;
+        
+        ChatHelper.SendMessage($"/gearset change {gearsetID + 1}");
+        return true;
+    }
+    
+    public static bool SwitchGearset(byte gearsetID)
+    {
+        if (Object == null) return false;
+        
+        var gearset = RaptureGearsetModule.Instance()->GetGearset(gearsetID);
+        if (gearset == null                                                  ||
+            !gearset->Flags.HasFlag(RaptureGearsetModule.GearsetFlag.Exists) ||
+            gearset->Flags.HasFlag(RaptureGearsetModule.GearsetFlag.MainHandMissing))
+            return false;
+        
+        ChatHelper.SendMessage($"/gearset change {gearsetID + 1}");
+        return true;
+    }
 }
