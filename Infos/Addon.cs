a40f3dff3c478599fc1125f0c066de81e09c10bf
AtmoOmen <atmoomen2004@outlook.com>
2025-08-10 22:27:24 +0800

    more addon

--- Infos/Addon.cs
+++ Infos/Addon.cs
@@ -21,6 +21,7 @@ public static unsafe partial class InfosOm
     public static AtkUnitBase* MainCross              => GetAddonByName("_MainCross");
     public static AtkUnitBase* Exp                    => GetAddonByName("_Exp");
     public static AtkUnitBase* NaviMap                => GetAddonByName("_NaviMap");
+    public static AtkUnitBase* CharaSelectRemain      => GetAddonByName("_CharaSelectRemain");
 
     #endregion
     
