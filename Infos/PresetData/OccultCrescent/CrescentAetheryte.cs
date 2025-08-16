39334e9679b62fa6c994a30692af189dff857105
AtmoOmen <atmoomen2004@outlook.com>
2025-08-16 16:33:38 +0800

    Crescent

--- Infos/PresetData/OccultCrescent/CrescentAetheryte.cs
+++ Infos/PresetData/OccultCrescent/CrescentAetheryte.cs
@@ -7,40 +7,37 @@ namespace OmenTools.Infos;
 /// <summary>
 /// 新月岛中的 魔路 / 简易魔路 数据类
 /// </summary>
-/// <param name="dataID">PlaceName ID</param>
-/// <param name="position">位置 (为传送到目标魔路后玩家所在位置, 非魔路对象位置)</param>
-/// <param name="point">类型</param>
-public class OccultAetheryte(uint dataID, Vector3 position, OccultAetherytePoint point) : IEquatable<OccultAetheryte>
+public class CrescentAetheryte(uint dataID, Vector3 position, CrescentAetherytePoint point) : IEquatable<CrescentAetheryte>
 {
     /// <summary>
     /// 调查队营地
     /// </summary>
-    public static OccultAetheryte ExpeditionBaseCamp { get; } = new(4927, new(834.0f, 73.0f, -694.6f), OccultAetherytePoint.ExpeditionBaseCamp);
+    public static CrescentAetheryte ExpeditionBaseCamp { get; } = new(4927, new(834.0f, 73.0f, -694.6f), CrescentAetherytePoint.ExpeditionBaseCamp);
 
     /// <summary>
     /// 放浪神圣域遗迹
     /// </summary>
-    public static OccultAetheryte WanderersHaven { get; } = new(4928, new(-169, 6.5f, -609.1f), OccultAetherytePoint.WanderersHaven);
+    public static CrescentAetheryte WanderersHaven { get; } = new(4928, new(-169, 6.5f, -609.1f), CrescentAetherytePoint.WanderersHaven);
 
     /// <summary>
     /// 水晶洞窟
     /// </summary>
-    public static OccultAetheryte CrystallizedCaverns { get; } = new(4929, new(-354.4f, 100f, -120.2f), OccultAetherytePoint.CrystallizedCaverns);
+    public static CrescentAetheryte CrystallizedCaverns { get; } = new(4929, new(-354.4f, 100f, -120.2f), CrescentAetherytePoint.CrystallizedCaverns);
 
     /// <summary>
     /// 古树湿原
     /// </summary>
-    public static OccultAetheryte Eldergrowth { get; } = new(4930, new(302.9f, 103f, 305.4f), OccultAetherytePoint.Eldergrowth);
+    public static CrescentAetheryte Eldergrowth { get; } = new(4930, new(302.9f, 103f, 305.4f), CrescentAetherytePoint.Eldergrowth);
 
     /// <summary>
     /// 石塔水沼
     /// </summary>
-    public static OccultAetheryte Stonemarsh { get; } = new(4947, new(-384.9f, 97.4f, 277.1f), OccultAetherytePoint.Stonemarsh);
+    public static CrescentAetheryte Stonemarsh { get; } = new(4947, new(-384.9f, 97.4f, 277.1f), CrescentAetherytePoint.Stonemarsh);
 
     /// <summary>
     /// 南征之章中所有 魔路 / 简易魔路 数据
     /// </summary>
-    public static List<OccultAetheryte> SouthHornAetherytes { get; } =
+    public static List<CrescentAetheryte> SouthHornAetherytes { get; } =
     [
         ExpeditionBaseCamp, WanderersHaven, CrystallizedCaverns, Eldergrowth, Stonemarsh
     ];
@@ -51,7 +48,7 @@ public class OccultAetheryte(uint dataID, Vector3 position, OccultAetherytePoint
     /// <param name="destination">目的地位置</param>
     /// <param name="result">目的地魔路</param>
     /// <returns>是否成功获取可用的魔路</returns>
-    public static bool TryGetNearestSouthHorn(Vector3 destination, [NotNullWhen(true)] out OccultAetheryte? result)
+    public static bool TryGetNearestSouthHorn(Vector3 destination, [NotNullWhen(true)] out CrescentAetheryte? result)
     {
         result = null;
             
@@ -82,12 +79,31 @@ public class OccultAetheryte(uint dataID, Vector3 position, OccultAetherytePoint
         aetheryteTime = (Vector3.Distance(finalPos,    aetherytePos)         / 12f) + 10f;
         return directTime > aetheryteTime;
     }
-    
-    public string               Name     { get; } = LuminaWrapper.GetPlaceName(dataID);
-    public uint                 DataID   { get; } = dataID;
-    public Vector3              Position { get; } = position;
-    public OccultAetherytePoint Point    { get; } = point;
-    public byte                 Index    { get; } = (byte)((byte)point > 4 ? (byte)point - 4 : (byte)point);
+
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public string Name { get; } = LuminaWrapper.GetPlaceName(dataID);
+
+    /// <summary>
+    /// PlaceName ID
+    /// </summary>
+    public uint DataID { get; } = dataID;
+
+    /// <summary>
+    /// 传送后位置 (为传送到目标魔路后玩家所在位置, 非魔路对象位置)
+    /// </summary>
+    public Vector3 Position { get; } = position;
+
+    /// <summary>
+    /// 类型
+    /// </summary>
+    public CrescentAetherytePoint Point { get; } = point;
+
+    /// <summary>
+    /// 传送界面显示索引
+    /// </summary>
+    public byte Index { get; } = (byte)((byte)point > 4 ? (byte)point - 4 : (byte)point);
 
     /// <summary>
     /// 使用魔路传送至目标魔路, 需要当前传送界面已打开
@@ -102,23 +118,23 @@ public class OccultAetheryte(uint dataID, Vector3 position, OccultAetherytePoint
         return true;
     }
     
-    public bool Equals(OccultAetheryte? other) => 
+    public bool Equals(CrescentAetheryte? other) => 
         DataID == other.DataID;
 
     public override bool Equals(object? obj) => 
-        obj is OccultAetheryte other && Equals(other);
+        obj is CrescentAetheryte other && Equals(other);
 
     public override int GetHashCode() => 
         (int)DataID;
 
-    public static bool operator ==(OccultAetheryte left, OccultAetheryte right) => 
+    public static bool operator ==(CrescentAetheryte left, CrescentAetheryte right) => 
         left.Equals(right);
 
-    public static bool operator !=(OccultAetheryte left, OccultAetheryte right) => 
+    public static bool operator !=(CrescentAetheryte left, CrescentAetheryte right) => 
         !left.Equals(right);
 }
 
-public enum OccultAetherytePoint : byte
+public enum CrescentAetherytePoint : byte
 {
     /// <summary>
     /// 调查队营地
