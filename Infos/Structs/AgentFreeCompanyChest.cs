1ddfd62ba986e03725e32af28f90d4b53831d7fc
AtmoOmen <atmoomen2004@outlook.com>
2025-08-26 22:30:14 +0800

    AgentFreeCompanyChest

---
+++ Infos/Structs/AgentFreeCompanyChest.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+namespace OmenTools.Infos;
+
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct AgentFreeCompanyChest
+{
+    [FieldOffset(0)]
+    public AgentInterface AgentInterface;
+
+    [FieldOffset(6956)]
+    public InventoryType ContextInventoryType;
+
+    [FieldOffset(6960)]
+    public short ContextInventorySlot;
+    
+    public static AgentFreeCompanyChest* Instance() =>
+        (AgentFreeCompanyChest*)AgentModule.Instance()->GetAgentByInternalId(AgentId.FreeCompanyChest);
+
+    public InventoryItem* GetContextInventoryItem()
+    {
+        var manager = InventoryManager.Instance();
+        if (manager == null) return null;
+        
+        if (ContextInventoryType == InventoryType.Invalid) return null;
+
+        return manager->GetInventorySlot(ContextInventoryType, ContextInventorySlot);
+    }
+}
