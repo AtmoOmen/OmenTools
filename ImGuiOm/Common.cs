9d01051df9ea8b6cd441518fbc0db32d2b0d96f1
AtmoOmen <atmoomen2004@outlook.com>
2025-10-02 21:01:18 +0800

    RenderPlayerInfo

--- ImGuiOm/Common.cs
+++ ImGuiOm/Common.cs
@@ -1,10 +1,14 @@
 ﻿using System.Numerics;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Interface.Utility;
 
 namespace OmenTools.ImGuiOm;
 
 // 暂时不太好分类的
 public static partial class ImGuiOm
 {
+    private static readonly Dictionary<(string PlayerName, string WorldName), byte[]> RenderedPlayerInfos = [];
+    
     public static Vector4 GetGradientColor()
     {
         const float period = 1f;
@@ -15,5 +19,17 @@ public static partial class ImGuiOm
         var blue  = (MathF.Sin(2 * MathF.PI * (t + (2f / 3f))) + 1) / 2;
 
         return new Vector4(red, green, blue, 1f);
+    }
+
+    public static void RenderPlayerInfo(string name, string world)
+    {
+        using var group = ImRaii.Group();
+
+        ImGuiHelpers.SeStringWrapped(RenderedPlayerInfos.GetOrAdd((name, world), 
+                                                                  _ => new SeStringBuilder()
+                                                                       .AddText($"{name}")
+                                                                       .AddIcon(BitmapFontIcon.CrossWorld)
+                                                                       .AddText($"{world}")
+                                                                       .Encode()));
     }
 }
