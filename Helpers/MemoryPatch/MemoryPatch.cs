24b6ce95d8a016ae2edfec7cedae2725ef1cd7d3
AtmoOmen <atmoomen2004@outlook.com>
2025-12-14 17:55:37 +0800

    MemoryPatch

---
+++ Helpers/MemoryPatch/MemoryPatch.cs
@@ -0,0 +1,103 @@
+using System.Globalization;
+using Dalamud;
+
+namespace OmenTools.Helpers;
+
+public class MemoryPatch : IDisposable
+{
+    public nint   Address   { get; }
+    public string Signature { get; } = null!;
+    public byte[] NewBytes  { get; } = null!;
+    public byte[] OldBytes  { get; } = null!;
+    public bool   IsEnabled { get; private set; }
+
+    public bool IsValid => Address != nint.Zero;
+
+    private static readonly List<MemoryPatch> MemoryPatches = [];
+
+    public MemoryPatch(nint address, IReadOnlyCollection<byte?> bytes, bool startEnabled = false)
+    {
+        if (address == nint.Zero) return;
+
+        var (trimmedBytes, skip) = TrimBytes(bytes);
+        Address = address + skip;
+        SafeMemory.ReadBytes(Address, trimmedBytes.Length, out var oldBytes);
+        OldBytes = oldBytes;
+        NewBytes = MergeBytes(trimmedBytes, OldBytes);
+
+        MemoryPatches.Add(this);
+        if (startEnabled) 
+            Enable();
+    }
+
+    public MemoryPatch(nint address, string bytesString, bool startEnabled = false) : 
+        this(address, ParseByteString(bytesString), startEnabled) 
+    { }
+
+    public MemoryPatch(string sig, IReadOnlyCollection<byte?> bytes, nint offset = 0, bool startEnabled = false) :
+        this(Scan(sig) + offset, bytes, startEnabled) =>
+        Signature = sig;
+
+    public MemoryPatch(string sig, string bytesString, nint offset = 0, bool startEnabled = false) : 
+        this(sig, ParseByteString(bytesString), offset, startEnabled)
+    { }
+
+    public void Enable()
+    {
+        if (IsEnabled || !IsValid) return;
+        SafeMemory.WriteBytes(Address, NewBytes);
+        IsEnabled = true;
+    }
+
+    public void Disable()
+    {
+        if (!IsEnabled || !IsValid) return;
+        SafeMemory.WriteBytes(Address, OldBytes);
+        IsEnabled = false;
+    }
+
+    public void Toggle() => 
+        Set(!IsEnabled);
+
+    public void Set(bool enable) => 
+        (enable ? (Action)Enable : Disable)();
+    
+    public virtual void Dispose()
+    {
+        Disable();
+        GC.SuppressFinalize(this);
+    }
+
+    internal static void DisposeAll() => 
+        MemoryPatches.ForEach(patch => patch.Dispose());
+
+    #region 私有
+
+    private static nint Scan(string sig)
+    {
+        if (DService.SigScanner.TryScanModule(sig, out var address))
+            return address;
+
+        DService.Log.Warning($"未能为 MemoryPatch 搜寻到签名 {sig}");
+        return nint.Zero;
+    }
+
+    private static byte?[] ParseByteString(string bytesString) =>
+        bytesString.Replace(" ", "")
+                   .Chunk(2)
+                   .Select(chunk => chunk.SequenceEqual("??") || chunk.SequenceEqual("**")
+                                        ? (byte?)null
+                                        : byte.Parse(new string(chunk), NumberStyles.HexNumber))
+                   .ToArray();
+
+    private static (byte?[] TrimmedBytes, int Skip) TrimBytes(IReadOnlyCollection<byte?> bytes)
+    {
+        var trimmedBytes = bytes.SkipWhile(b => !b.HasValue).ToArray();
+        return (trimmedBytes, bytes.Count - trimmedBytes.Length);
+    }
+
+    private static byte[] MergeBytes(byte?[] newBytes, byte[] oldBytes) =>
+        newBytes.Select((b, i) => b ?? oldBytes[i]).ToArray();
+
+    #endregion
+}
---
+++ Helpers/MemoryPatch/MemoryPatchWithPointer.cs
@@ -0,0 +1,59 @@
+﻿using Dalamud;
+using Dalamud.Memory;
+
+namespace OmenTools.Helpers;
+
+public class MemoryPatchWithPointer<T> : MemoryPatch
+    where T : unmanaged
+{
+    public nint PointerAddress { get; }
+    public T    OriginalValue { get; private set; }
+    public T    CurrentValue  { get; private set; }
+
+    private bool isPatched;
+
+    public void Set(T value)
+    {
+        if (!isPatched)
+        {
+            if (!SafeMemory.Read<T>(PointerAddress, out var result)) return;
+            
+            OriginalValue = result;
+            isPatched     = true;
+        }
+
+        if (!SafeMemory.Write(PointerAddress, value)) return;
+        CurrentValue = value;
+    }
+
+    public void Reset()
+    {
+        if (!isPatched) return;
+        if (!SafeMemory.Write(PointerAddress, OriginalValue)) return;
+        
+        CurrentValue = OriginalValue;
+        isPatched    = false;
+    }
+
+    public override void Dispose()
+    {
+        Reset();
+        base.Dispose();
+    }
+    
+    public MemoryPatchWithPointer(nint address, IReadOnlyCollection<byte?> bytes, nint pointerOffset = 0, bool startEnabled = false) 
+        : base(address, bytes, startEnabled) => 
+        PointerAddress = address + pointerOffset;
+
+    public MemoryPatchWithPointer(nint address, string bytesString, nint pointerOffset = 0, bool startEnabled = false) 
+        : base(address, bytesString, startEnabled) => 
+        PointerAddress = address + pointerOffset;
+
+    public MemoryPatchWithPointer(string sig, IReadOnlyCollection<byte?> bytes, nint scanOffset = 0, nint pointerOffset = 0, bool startEnabled = false) 
+        : base(sig, bytes, scanOffset, startEnabled) => 
+        PointerAddress = Address + pointerOffset;
+
+    public MemoryPatchWithPointer(string sig, string bytesString, nint scanOffset = 0, nint pointerOffset = 0, bool startEnabled = false) 
+        : base(sig, bytesString, scanOffset, startEnabled) => 
+        PointerAddress = Address + pointerOffset;
+}
