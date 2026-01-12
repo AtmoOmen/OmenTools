3c0cac4b191ff7df12ec7082a8a1f42b3a16e0f0
AtmoOmen <atmoomen2004@outlook.com>
2026-01-12 22:06:55 +0800

    删除复杂的线程切换

--- Managers/FrameworkManager.cs
+++ Managers/FrameworkManager.cs
@@ -8,12 +8,8 @@ namespace OmenTools.Managers;
 
 public class FrameworkManager : OmenServiceBase<FrameworkManager>
 {
-    private readonly CancellationTokenSource cancelSource = new();
+    private readonly ConcurrentDictionary<IFramework.OnUpdateDelegate, (uint Throttle, string HashCode)> methodsCollection  = [];
 
-    private readonly ConcurrentDictionary<IFramework.OnUpdateDelegate, (uint Throttle, string HashCode)> methodsCollectionSync  = [];
-    private readonly ConcurrentDictionary<IFramework.OnUpdateDelegate, (uint Throttle, string HashCode)> methodsCollectionAsync = [];
-
-    private int isLastAsyncUpdating;
 
     internal override void Init() =>
         DService.Instance().Framework.Update += DailyRoutines_OnUpdate;
@@ -22,38 +18,18 @@ public class FrameworkManager : OmenServiceBase<FrameworkManager>
     {
         DService.Instance().Framework.Update -= DailyRoutines_OnUpdate;
 
-        cancelSource.Cancel();
-        cancelSource.Dispose();
-
-        methodsCollectionSync.Clear();
-        methodsCollectionAsync.Clear();
+        methodsCollection.Clear();
     }
 
-    public bool Reg(IFramework.OnUpdateDelegate method, bool isSync = false, uint throttleMS = 0)
-    {
-        var state = false;
-        switch (isSync)
-        {
-            case false:
-                if (methodsCollectionAsync.TryAdd(method, (throttleMS, $"{RuntimeHelpers.GetHashCode(method)}_{method.Method.MethodHandle.Value}")))
-                    state = true;
-                break;
-            case true:
-                if (methodsCollectionSync.TryAdd(method, (throttleMS, $"{RuntimeHelpers.GetHashCode(method)}_{method.Method.MethodHandle.Value}")))
-                    state = true;
-                break;
-        }
-        
-        return state;
-    }
+    public bool Reg(IFramework.OnUpdateDelegate method, uint throttleMS = 0) =>
+        methodsCollection.TryAdd(method, (throttleMS, $"{RuntimeHelpers.GetHashCode(method)}_{method.Method.MethodHandle.Value}"));
 
     public bool Unreg(params IFramework.OnUpdateDelegate[] methods)
     {
         var state = true;
         foreach (var method in methods)
         {
-            if (!methodsCollectionAsync.TryRemove(method, out _) &&
-                !methodsCollectionSync.TryRemove(method, out _)) 
+            if (!methodsCollection.TryRemove(method, out _)) 
                 state = false;
         }
 
@@ -62,35 +38,7 @@ public class FrameworkManager : OmenServiceBase<FrameworkManager>
 
     private void DailyRoutines_OnUpdate(IFramework framework)
     {
-        framework.Run(() =>
-        {
-            if (Interlocked.Exchange(ref isLastAsyncUpdating, 1) != 0)
-                return;
-
-            try
-            {
-                foreach (var (method, (throttle, key)) in methodsCollectionAsync)
-                {
-                    if (throttle > 0 && !Throttler.Throttle($"FrameworkManager-OnUpdate-{key}", throttle))
-                        continue;
-
-                    try
-                    {
-                        method(framework);
-                    }
-                    catch (Exception ex)
-                    {
-                        Error("在 Framework 异步更新过程中发生错误", ex);
-                    }
-                }
-            }
-            finally
-            {
-                Interlocked.Exchange(ref isLastAsyncUpdating, 0);
-            }
-        }, cancellationToken: cancelSource.Token).ConfigureAwait(false);
-        
-        foreach (var (method, (throttle, hashCode)) in methodsCollectionSync)
+        foreach (var (method, (throttle, hashCode)) in methodsCollection)
         {
             if (throttle > 0 && !Throttler.Throttle($"FrameworkManager-OnUpdate-{hashCode}", throttle))
                 continue;
