1208d1fd94be8f28e1bb1d8e1fcda45a74944019
AtmoOmen <atmoomen2004@outlook.com>
2026-01-31 10:10:37 +0800

    GetIconID

--- Extensions/AtkExtension.cs
+++ Extensions/AtkExtension.cs
@@ -7,6 +7,31 @@ namespace OmenTools.Extensions;
 // 单独开一个文件方法太少了，就会扔到这里
 public static unsafe class AtkExtension
 {
+    extension(scoped ref AtkImageNode node)
+    {
+        public uint GetIconID()
+        {
+            fixed (AtkImageNode* ptr = &node)
+            {
+                if (ptr == null) return 0;
+
+                var partList = ptr->PartsList;
+                if (partList == null) return 0;
+
+                var parts = partList->Parts;
+                if (parts == null) return 0;
+                
+                var asset = parts[ptr->PartId].UldAsset;
+                if (asset == null) return 0;
+
+                var resource = asset->AtkTexture.Resource;
+                if (resource == null) return 0;
+                
+                return resource->IconId;
+            }
+        }
+    }
+    
     extension(scoped ref AtkTextNode node)
     {
         public Vector2 GetTextDrawSize(bool considerScale = false)
