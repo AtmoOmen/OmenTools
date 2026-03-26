44ea7fe4f32883561095d9e6b7727b63754e0d39
AtmoOmen <atmoomen2004@outlook.com>
2026-03-26 22:42:20 +0800

    调整为 40f

--- ImGuiOm/Widgets.cs
+++ ImGuiOm/Widgets.cs
@@ -1,5 +1,3 @@
-using Dalamud.Interface;
-
 namespace OmenTools.ImGuiOm;
 
 public static partial class ImGuiOm
@@ -54,24 +52,26 @@ public static partial class ImGuiOm
         }
     }
 
-    public static void HelpMarker(
-        string tooltip,
-        float warpPos = 20f,
-        FontAwesomeIcon icon = FontAwesomeIcon.InfoCircle,
-        bool useStaticFont = false)
+    public static void HelpMarker
+    (
+        string          tooltip,
+        float           warpPos       = 40f,
+        FontAwesomeIcon icon          = FontAwesomeIcon.InfoCircle,
+        bool            useStaticFont = false
+    )
     {
         ImGui.SameLine();
-        
+
         using var group = ImRaii.Group();
-        
+
         using (ImRaii.PushFont(UiBuilder.IconFont, useStaticFont))
         {
             var origPosY = ImGui.GetCursorPosY();
-            ImGui.SetCursorPosY(origPosY + (ImGui.GetStyle().FramePadding.Y * 0.5f));
+            ImGui.SetCursorPosY(origPosY + ImGui.GetStyle().FramePadding.Y * 0.5f);
             ImGui.TextDisabled(icon.ToIconString());
             ImGui.SetCursorPosY(origPosY);
         }
-        
+
         if (ImGui.IsItemHovered())
         {
             using (ImRaii.Tooltip())
