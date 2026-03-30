a6e68741ad64e1b3d09d7ac0f9ef7d0534b7be46
AtmoOmen <atmoomen2004@outlook.com>
2026-03-31 00:44:36 +0800

    支持直接传managed

--- Extensions/AgentExtension.cs
+++ Extensions/AgentExtension.cs
@@ -9,10 +9,17 @@ public static unsafe class AgentExtension
 {
     extension(AgentId agentID)
     {
-        public AtkValue* SendEvent(ulong eventKind, params object[] eventparams)
+        public AtkValue* SendEvent(ulong eventKind, params object[] eventParams)
         {
             var agent = AgentModule.Instance()->GetAgentByInternalId(agentID);
-            return agent == null ? null : agent->SendEvent(eventKind, eventparams);
+            return agent == null ? null : agent->SendEvent(eventKind, eventParams);
+        }
+        
+        /// <remarks>请自行处理处理传入的 AtkValueArray 生命周期</remarks>
+        public AtkValue* SendEvent(ulong eventKind, scoped in AtkValueArray valueArray)
+        {
+            var agent = AgentModule.Instance()->GetAgentByInternalId(agentID);
+            return agent == null ? null : agent->SendEvent(eventKind, valueArray);
         }
     }
 
@@ -23,11 +30,25 @@ public static unsafe class AgentExtension
             var returnValue = stackalloc AtkValue[1];
             return agentInterface.SendEvent(returnValue, eventKind, eventParams);
         }
+        
+        /// <remarks>请自行处理处理传入的 AtkValueArray 生命周期</remarks>
+        public AtkValue* SendEvent(ulong eventKind, scoped in AtkValueArray valueArray)
+        {
+            var returnValue = stackalloc AtkValue[1];
+            return agentInterface.SendEvent(returnValue, eventKind, valueArray);
+        }
 
         public AtkValue* SendEvent(AtkValue* returnValue, ulong eventKind, params object[] eventParams)
         {
             using var atkValues = new AtkValueArray(eventParams);
             agentInterface.ReceiveEvent(returnValue, atkValues, (uint)eventParams.Length, eventKind);
+            return returnValue;
+        }
+        
+        /// <remarks>请自行处理处理传入的 AtkValueArray 生命周期</remarks>
+        public AtkValue* SendEvent(AtkValue* returnValue, ulong eventKind, scoped in AtkValueArray valueArray)
+        {
+            agentInterface.ReceiveEvent(returnValue, valueArray, (uint)valueArray.Length, eventKind);
             return returnValue;
         }
     }
