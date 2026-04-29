e63a09e3c9c7b975a6b4d9bdaf980d0321867f02
AtmoOmen <atmoomen2004@outlook.com>
2026-04-29 15:15:57 +0800

    API 15

--- Interop/Game/Models/Packets/Downstream/OutOnALimbPacket.cs
+++ Interop/Game/Models/Packets/Downstream/OutOnALimbPacket.cs
@@ -1,10 +1,12 @@
 using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.Network;
 using OmenTools.Interop.Game.Models.Packets.Abstractions;
 
 namespace OmenTools.Interop.Game.Models.Packets.Downstream;
 
 /// <summary>
 ///     金碟游戏机“孤树无援”游玩结果
+///     <seealso cref="PacketDispatcher.Delegates.HandleEventYieldPacket" />
 /// </summary>
 [StructLayout(LayoutKind.Explicit)]
 public struct OutOnALimbPacket : IDownstreamPacket
@@ -21,15 +23,6 @@ public struct OutOnALimbPacket : IDownstreamPacket
     public string Log() =>
         $"孤树无援游玩结果包体\n" +
         $"游玩结果: {Result} | 体力: {Health} | 奖励等级: {BonusLevel}";
-
-
-    public static CompSig Signature { get; } =
-        new
-        (
-            "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 49 8B D9 41 0F B6 F8 0F B7 F2 8B E9 E8 ?? ?? ?? ?? 44 0F B6 54 24 ?? 44 0F B6 CF 44 88 54 24 ?? 44 0F B7 C6 8B D5"
-        );
-
-    public unsafe delegate void* Delegate(uint eventID, ushort category, ushort stage, OutOnALimbPacket* packet, byte a5);
 }
 
 public enum OutOnALimbResult : uint
