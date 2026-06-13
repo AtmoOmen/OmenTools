89bd803d87ff863fd58bbf0266074dbddc1f1d09
AtmoOmen <atmoomen2004@outlook.com>
2026-06-13 17:08:27 +0800

    更新一个托管的

--- Extensions/EventObjectManagerExtension.cs
+++ Extensions/EventObjectManagerExtension.cs
@@ -1,21 +1,21 @@
-﻿using FFXIVClientStructs.FFXIV.Client.Game.Character;
+﻿using FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 namespace OmenTools.Extensions;
 
-public static unsafe class CharacterManagerExtension
+public static unsafe class EventObjectManagerExtension
 {
-    extension(scoped ref CharacterManager manager)
+    extension(scoped ref EventObjectManager manager)
     {
         /// <summary>
         ///     需传入 static 方法指针
         /// </summary>
-        public BattleChara* FindFirst(delegate* managed<BattleChara*, bool> predicate)
+        public GameObject* FindFirst(delegate* managed<GameObject*, bool> predicate)
         {
-            fixed (CharacterManager* ptr = &manager)
+            fixed (EventObjectManager* ptr = &manager)
             {
                 if (ptr == null) return null;
 
-                foreach (var p in ptr->BattleCharas)
+                foreach (var p in ptr->EventObjects)
                 {
                     if (p.Value == null) continue;
                     if (predicate(p.Value))
@@ -25,14 +25,14 @@ public static unsafe class CharacterManagerExtension
 
             return null;
         }
-        
-        public BattleChara* FindFirst(Func<nint, bool> predicate)
+
+        public GameObject* FindFirst(Func<nint, bool> predicate)
         {
-            fixed (CharacterManager* ptr = &manager)
+            fixed (EventObjectManager* ptr = &manager)
             {
                 if (ptr == null) return null;
 
-                foreach (var p in ptr->BattleCharas)
+                foreach (var p in ptr->EventObjects)
                 {
                     if (p.Value == null) continue;
                     if (predicate((nint)p.Value))
