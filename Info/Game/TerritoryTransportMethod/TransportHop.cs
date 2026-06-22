1ec0402bc0f4cd3c580e3cb4cb65697ef8cc0d1a
AtmoOmen <atmoomen2004@outlook.com>
2026-06-22 18:12:07 +0800

    移除过度抽象

--- Info/Game/TerritoryTransportMethod/TransportHop.cs
+++
@@ -1,9 +0,0 @@
-using OmenTools.Info.Game.TerritoryTransportMethod.Abstractions;
-
-namespace OmenTools.Info.Game.TerritoryTransportMethod;
-
-public sealed record TransportHop
-(
-    TerritoryTransportMethodBase Method,
-    uint                         TargetTerritory
-);
