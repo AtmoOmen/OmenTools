873dbc5050819f1b09adc40629cf0727d0e1e5ae
AtmoOmen <atmoomen2004@outlook.com>
2026-09-23 12:33:09 +0800

    AgentWorldTravelExtension

---
+++ Extensions/AgentWorldTravelExtension.cs
@@ -0,0 +1,34 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+namespace OmenTools.Extensions;
+
+public static unsafe class AgentWorldTravelExtension
+{
+    extension
+    (
+        scoped ref AgentWorldTravel agent
+    )
+    {
+        public void TravelTo
+        (
+            uint destinationWorldID
+        ) =>
+            agent.TravelTo((ushort)destinationWorldID);
+
+        public void TravelTo
+        (
+            ushort destinationWorldID
+        )
+        {
+            fixed (AgentWorldTravel* ptr = &agent)
+            {
+                if (ptr == null)
+                    return;
+
+                ptr->DestinationWorldId = destinationWorldID;
+                ptr->SetupWorldTravelInfo((ushort)GameState.CurrentWorld, destinationWorldID);
+                AgentId.WorldTravel.SendEvent(1, 0);
+            }
+        }
+    }
+}
