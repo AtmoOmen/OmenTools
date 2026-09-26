a6e0c50a721d9cee61fc74ccc256000c90d59abe
AtmoOmen <atmoomen2004@outlook.com>
2026-09-27 00:34:12 +0800

    支持多参

--- OmenService/Implementations/Managers/ContextMenu/Models/ContextMenuEntryInfo.cs
+++ OmenService/Implementations/Managers/ContextMenu/Models/ContextMenuEntryInfo.cs
@@ -4,7 +4,9 @@ namespace OmenTools.OmenService;
 
 public sealed class ContextMenuEntryInfo : ContextMenuEntry
 {
-    private readonly Func<ContextMenuOpenedArgs, ContextMenuItem?> create;
+    private readonly Func<ContextMenuOpenedArgs, ContextMenuItem?>? create;
+
+    private readonly Func<ContextMenuOpenedArgs, IReadOnlyList<ContextMenuItem>?>? createMultiple;
 
     public ContextMenuEntryInfo
     (
@@ -25,6 +27,25 @@ public sealed class ContextMenuEntryInfo : ContextMenuEntry
         create     = createFunc;
     }
 
+    public ContextMenuEntryInfo
+    (
+        string                                                       identifier,
+        Func<ContextMenuOpenedArgs, IReadOnlyList<ContextMenuItem>?> createFunc,
+        int?                                                         priority   = null,
+        bool                                                         omitPrefix = false,
+        ReadOnlySeString?                                            prefix     = null
+    )
+    {
+        ArgumentException.ThrowIfNullOrEmpty(identifier);
+        ArgumentNullException.ThrowIfNull(createFunc);
+
+        Identifier     = identifier;
+        Priority       = priority;
+        OmitPrefix     = omitPrefix;
+        Prefix         = prefix;
+        createMultiple = createFunc;
+    }
+
     public override string Identifier { get; }
 
     public override int? Priority { get; }
@@ -37,5 +58,11 @@ public sealed class ContextMenuEntryInfo : ContextMenuEntry
     (
         ContextMenuOpenedArgs args
     ) =>
-        create(args);
+        create?.Invoke(args);
+
+    public override IReadOnlyList<ContextMenuItem>? CreateMultiple
+    (
+        ContextMenuOpenedArgs args
+    ) =>
+        createMultiple?.Invoke(args);
 }
